97a37bfd3e4ba4ebfb07e37964cab6350e799cf4
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-13 10:35:51 -0300

    Fixed with Max's solution

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("You got to the first middleware succesufully");
-  next();
-});
-
+// The path needs the more specific middleware first
 app.use("/users", (req, res, next) => {
   console.log("You got to the second middleware succesufully");
-  res.send("<h1>You have no users yet!</h1>");
-  res.end();
+  res.send("<h1>This is the users serves.</h1>");
+});
+
+// The path needs the less specific middleware at last
+app.use("/", (req, res, next) => {
+  console.log("You got to the first middleware succesufully");
+  res.send("This is just the generic server.");
 });
 
 app.listen(3000);
