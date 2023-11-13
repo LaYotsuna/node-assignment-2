345fda1b813438b721f70b568b774c68eb71aa5b
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-13 10:36:57 -0300

    Last adjustments

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
 // The path needs the more specific middleware first
 app.use("/users", (req, res, next) => {
-  console.log("You got to the second middleware succesufully");
-  res.send("<h1>This is the users serves.</h1>");
+  console.log("You got to the second middleware successfully");
+  res.send("This is the users server.");
 });
 
 // The path needs the less specific middleware at last
 app.use("/", (req, res, next) => {
-  console.log("You got to the first middleware succesufully");
+  console.log("You got to the first middleware successfully");
   res.send("This is just the generic server.");
 });
 
