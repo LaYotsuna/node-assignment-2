6ed48bf5d534a0c6828e999c5317a9c296b84973
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-13 10:16:20 -0300

    Implementing middlewares with Express

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require("express");
 
 const app = express();
 
-app;
+app.use("/", (req, res, next) => {
+  console.log("You got to the first middleware succesufully");
+  res.send("<h1>Redirecting to users...</h1>");
+  setTimeout(next(), 3000);
+});
+
+app.use("/users", (req, res, next) => {
+  console.log("You got to the second middleware succesufully");
+  res.send("<h1>You have nmo users yet!</h1>");
+});
 
 app.listen(3000);
